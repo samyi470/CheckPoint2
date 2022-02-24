8cc3ef1a28534db7698d310e2e4f09658e92f707
Sam Yi <samuelyi806@gmail.com>
2022-02-23 20:52:39 -0800

    Basic Template

--- checkPointMng/models.py
+++ checkPointMng/models.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.db import models
 
 # following commands after making any changes:
@@ -12,3 +13,32 @@ class MainMenu(models.Model):
 
     def __str__(self):
         return self.item
+
+
+LAX_TERMINALS = (
+    ("Suites", "Suites"),
+    ("TBIT Main Checkpoint", "TBIT Main Checkpoint"),
+    ("Terminal 1 - Passenger", "Terminal 1 - Passenger"),
+    ("Terminal 2 - Passenger", "Terminal 2 - Passenger"),
+    ("Terminal 3 - Passenger", "Terminal 3 - Passenger"),
+    ("Terminal 4 - FIS", "Terminal 4 - FIS"),
+    ("Terminal 4 - Passenger", "Terminal 4 - Passenger"),
+    ("Terminal 4a - Passenger", "Terminal 4a - Passenger"),
+    ("Terminal 5 - Passenger", "Terminal 5 - Passenger"),
+    ("Terminal 5a - Passenger", "Terminal 5a - Passenger"),
+    ("Terminal 6 - Passenger", "Terminal 6 - Passenger"),
+    ("Terminal 7 - Passenger", "Terminal 7 - Passenger"),
+)
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class Search(models.Model):
+    terminal = models.CharField(max_length=23, choices=LAX_TERMINALS)
+    start = models.DateField()
+    end = models.DateField()
+
+    def __str__(self):
+        return str(self.id)
--- checkPointMng/views.py
+++ checkPointMng/views.py
@@ -1,7 +1,73 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Search
+from .forms import SearchForm
+import datetime
 
 
 # hello world test
 def index(request):
-    return render(request, 'checkPointMng/home.html')
+    submitted = False
+    terminal = ''
+    start = ''
+    startYear = ''
+    startMonth = ''
+    startDay = ''
+    end = ''
+    endYear = ''
+    endMonth = ''
+    endDay = ''
+
+    # if (3. form submitted from .html (POST))
+    if request.method == 'POST':
+        # set form from POST request
+        form = SearchForm(request.POST, request.FILES)
+
+        # if valid, save form and return with GET parameter
+        if form.is_valid():
+            terminal = request.POST.get('terminal')
+            start = request.POST.get('start')
+            end = request.POST.get('end')
+
+            return HttpResponseRedirect('/?submitted=True&terminal=' + terminal + '&start=' + start + '&end=' + end)
+
+    # else (GET) (1. display an empty form to be filled out for the first time),
+    else:
+        form = SearchForm()
+
+        # if (4. submitted is passed as GET parameter, set submitted to true)
+        if 'submitted' in request.GET:
+            submitted = True
+            terminal = request.GET.get('terminal')
+            start = request.GET.get('start')
+            end = request.GET.get('end')
+            startDate = datetime.datetime.strptime(start, '%Y-%m-%d')
+            endDate = datetime.datetime.strptime(end, '%Y-%m-%d')
+
+            # to utilize ISO calendar - startDate.year/month/day
+
+            startYear = startDate.strftime('%Y')
+            startMonth = startDate.strftime('%m')
+            startDay = startDate.strftime('%d')
+            endYear = endDate.strftime('%Y')
+            endMonth = endDate.strftime('%m')
+            endDay = endDate.strftime('%d')
+
+    # (2., 5. render .html page)
+    return render(request,
+                  'checkPointMng/home.html',
+                  {
+                      'form': form,
+                      'submitted': submitted,
+                      'terminal': terminal,
+                      'start': start,
+                      'startYear': startYear,
+                      'startMonth': startMonth,
+                      'startDay': startDay,
+                      'end': end,
+                      'endYear': endYear,
+                      'endMonth': endMonth,
+                      'endDay': endDay,
+                  })
+
+    # return render(request, 'checkPointMng/home.html')
