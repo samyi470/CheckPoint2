5345b782f023cafaa888cd9bf0c36c98975069b6
Sam Yi <samuelyi806@gmail.com>
2022-02-27 01:32:40 -0800

    Django-Crispy setup
    Setup Django-Crispy and put together basic form with styling

--- checkPointMng/forms.py
+++ checkPointMng/forms.py
@@ -2,8 +2,27 @@ from django import forms
 from django.forms import ModelForm
 from .models import Search, DateInput
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Column, Field
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+
 
 class SearchForm(ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
+    helper.layout = Layout(
+        Row(
+            Column('terminal', css_class='form-group col-6'),
+            Column('start', css_class='form-group col-3'),
+            Column('end', css_class='form-group col-3'),
+            css_class='form-row'
+        ),
+        Div(
+            Submit('submit', 'Submit'),
+            css_class='d-flex justify-content-center'
+        )
+    )
+
     class Meta:
         model = Search
         widgets = {'start': DateInput(), 'end': DateInput()}
